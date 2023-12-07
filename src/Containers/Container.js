00ae2c48a2ccc3281602e5098c63ea1b61246a73
Alpacorp <alejandro.palacios88@gmail.com>
2023-12-07 01:19:09 -0500

    remove: remove fragment into layout component

--- src/Containers/Container.js
+++ src/Containers/Container.js
@@ -26,34 +26,32 @@ import Layout from "./Layout";
 
 const Container = () => {
   return (
-    <>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/stepone" element={<StepOne />} />
-          <Route path="/steptwo" element={<StepTwo />} />
-          <Route path="/stepthree" element={<StepThree />} />
-          <Route path="/stepfour" element={<StepFour />} />
-          <Route path="/stepfive" element={<StepFive />} />
-          <Route path="/stepsix" element={<StepSix />} />
-          <Route path="/stepseven" element={<StepSeven />} />
-          <Route path="/anton" element={<Anton />} />
-          <Route path="/tutaina" element={<Tutaina />} />
-          <Route path="/nanita" element={<Nanita />} />
-          <Route path="/campana" element={<Campana />} />
-          <Route path="/peces" element={<Peces />} />
-          <Route path="/dayone" element={<DayOne />} />
-          <Route path="/daytwo" element={<DayTwo />} />
-          <Route path="/daythree" element={<DayThree />} />
-          <Route path="/dayfour" element={<DayFour />} />
-          <Route path="/dayfive" element={<DayFive />} />
-          <Route path="/daysix" element={<DaySix />} />
-          <Route path="/dayseven" element={<DaySeven />} />
-          <Route path="/dayeight" element={<DayEigth />} />
-          <Route path="/daynine" element={<DayNine />} />
-        </Routes>
-      </Layout>
-    </>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/stepone" element={<StepOne />} />
+        <Route path="/steptwo" element={<StepTwo />} />
+        <Route path="/stepthree" element={<StepThree />} />
+        <Route path="/stepfour" element={<StepFour />} />
+        <Route path="/stepfive" element={<StepFive />} />
+        <Route path="/stepsix" element={<StepSix />} />
+        <Route path="/stepseven" element={<StepSeven />} />
+        <Route path="/anton" element={<Anton />} />
+        <Route path="/tutaina" element={<Tutaina />} />
+        <Route path="/nanita" element={<Nanita />} />
+        <Route path="/campana" element={<Campana />} />
+        <Route path="/peces" element={<Peces />} />
+        <Route path="/dayone" element={<DayOne />} />
+        <Route path="/daytwo" element={<DayTwo />} />
+        <Route path="/daythree" element={<DayThree />} />
+        <Route path="/dayfour" element={<DayFour />} />
+        <Route path="/dayfive" element={<DayFive />} />
+        <Route path="/daysix" element={<DaySix />} />
+        <Route path="/dayseven" element={<DaySeven />} />
+        <Route path="/dayeight" element={<DayEigth />} />
+        <Route path="/daynine" element={<DayNine />} />
+      </Routes>
+    </Layout>
   );
 };
 
